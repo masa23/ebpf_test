59522da4973f38380786912b46998a3b1260169a
Masafumi Yamamoto <masa23@gmail.com>
2022-04-26 06:39:48 +0000

    update

--- src/xdp.c
+++ src/xdp.c
@@ -8,14 +8,14 @@ char __license[] SEC("license") = "Dual MIT/GPL";
 
 struct bpf_map_def SEC("maps") xdp_map = {
     .type = BPF_MAP_TYPE_HASH,
-	.key_size = sizeof(__u32),
-	.value_size = sizeof(__u32),
-	.max_entries = 1000,
-    .map_flags = 0
-};
+    .key_size = sizeof(__u32),
+    .value_size = sizeof(__u32),
+    .max_entries = 1000,
+    .map_flags = 0};
 
 SEC("xdp")
-int xdp_prog(struct xdp_md *ctx) {
+int xdp_prog(struct xdp_md *ctx)
+{
     void *data_end = (void *)(long)ctx->data_end;
     void *data = (void *)(long)ctx->data;
     __u32 zero = 0;
@@ -26,20 +26,23 @@ int xdp_prog(struct xdp_md *ctx) {
     if (data + nh_off > data_end)
         return XDP_PASS;
 
-    if(eth->h_proto != bpf_htons(ETH_P_IP))
+    if (eth->h_proto != bpf_htons(ETH_P_IP))
         return XDP_PASS;
 
     struct iphdr *iph = data + nh_off;
-    if ((void*)&iph[1] > data_end)
+    if ((void *)&iph[1] > data_end)
         return XDP_PASS;
-    
+
     __u32 protocol = iph->protocol;
-    if (protocol == 1) {
+    if (protocol == 1)
+    {
         value = bpf_map_lookup_elem(&xdp_map, &iph->saddr);
-        if(!value){
+        if (!value)
+        {
             bpf_map_update_elem(&xdp_map, &iph->saddr, &zero, BPF_NOEXIST);
             value = bpf_map_lookup_elem(&xdp_map, &iph->saddr);
-            if(!value) {
+            if (!value)
+            {
                 return XDP_PASS;
             }
         }
